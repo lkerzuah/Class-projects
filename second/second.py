e6590659885bdeb35645eea44e8e826368d9d78b
Levis V. Kerzuah <lkerzuah@gmail.com>
2023-09-19 14:42:01 +0000

    This is my second application.

--- second/second.py
+++ second/second.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import plotly.express as px
+import numpy
 import pandas as pd
 import os
 import warnings
 warnings.filterwarnings("ignore")
 
-st.set_page_config(page_title="superstore", page_icon=":Bar_chart:", layout="wide")
+st.set_page_config(page_title="superstore",page_icon=":Bar_chart:",layout="wide")
 
 st.title(":bar_chart: My superstore EDA Application")
 st.markdown("<style>div.block-container{padding-top:1rem;</style>", unsafe_allow_html=True)
